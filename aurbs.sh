6e0d244d35613569ade31be4220b5ef468ab5c26
Emilien Devos <unixfox@users.noreply.github.com>
2017-01-28 18:39:47 +0100

    I AM SO STUPID

--- aurbs.sh
+++ aurbs.sh
@@ -7,5 +7,5 @@ else
     wget https://github.com/unixfox/aur/raw/master/aurbs.yml -O /etc/aurbs.yml
     curl -sS https://raw.githubusercontent.com/unixfox/aur/master/pkgs >> /etc/aurbs.yml
     aurbs > /var/log/aurbs/"$DATE".txt
-    lftp "ftp://"FTP_USER":"FTP_PASSWORD"@"FTP_HOST"" -e "mirror -R -e /var/lib/aurbs/public_repo/ / ; quit"
+    lftp "ftp://"$FTP_USER":"$FTP_PASSWORD"@"$FTP_HOST"" -e "mirror -R -e /var/lib/aurbs/public_repo/ / ; quit"
 fi
