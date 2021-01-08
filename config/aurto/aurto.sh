7e25804348f9e8f99b411dada43e8c58bd4772f9
Emilien Devos <contact@emiliendevos.be>
2021-01-08 22:53:06 +0100

    add frontail + aurto modifs

--- config/aurto/aurto.sh
+++ config/aurto/aurto.sh
@@ -3,5 +3,5 @@ IFS='
 '
 for x in `curl https://raw.githubusercontent.com/unixfox/aur/master/pkgs`; do
    pkg_name=$(echo $x | cut -c 3-)
-   aurto add $pkg_name
+   aurto add $pkg_name 2>&1 | tee -a /var/tmp/aurto.log
 done
